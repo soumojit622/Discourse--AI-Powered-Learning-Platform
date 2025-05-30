6ffbb0250d9900a42fd3afda6857d784ca311caa
Soumojit Banerjee <soumojitbanerjee22@gmail.com>
2025-05-30 12:10:56 +0530

    git commit -m "✨ feat: completed and styled responsive navbar"

---
+++ components/Navbar.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import NavItems from "./NavItems";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link href="/">
+        <div className="flex items-center gap-2.5 cursor-pointer">
+          <Image src="/images/logo.svg" alt="logo" width={116} height={112} />
+
+        </div>
+      </Link>
+      <div className="flex items-center gap-8">
+        <NavItems />
+        {/* <SignedOut> */}
+        {/* <SignInButton> */}
+        <button className="btn-signin">Sign In</button>
+        {/* </SignInButton> */}
+        {/* </SignedOut> */}
+        {/* <SignedIn>
+          <UserButton />
+        </SignedIn> */}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
