6fedeb07e927d0553bf7c7620769c5c42415b15b
Soumojit Banerjee <soumojitbanerjee22@gmail.com>
2025-05-31 00:37:45 +0530

    git commit -m "🔧 feat: implemented Supabase and connected it with Clerk auth"

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,14 +1,23 @@
 import CompanionForm from "@/components/CompanionForm";
-import React from "react";
+import { newCompanionPermissions } from "@/lib/actions/companion.actions";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const NewCompanion = () => {
+const NewCompanion = async () => {
+  const { userId } = await auth();
+  if (!userId) redirect("/sign-in");
+
+  const canCreateCompanion = await newCompanionPermissions();
+
   return (
     <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
-      <article className="w-full gap-4 flex flex-col">
-        <h1>Companion Builder</h1>
+      
+        <article className="w-full gap-4 flex flex-col">
+          <h1>Companion Builder</h1>
 
-        <CompanionForm />
-      </article>
+          <CompanionForm />
+        </article>
+      
     </main>
   );
 };
--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -2,27 +2,28 @@
 
 import { Button } from "@/components/ui/button";
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
 } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
 import { subjects } from "@/constants";
+import { createCompanion } from "@/lib/actions/companion.actions";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { redirect } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-// import { createCompanion } from "@/lib/actions/companion.actions";
 
 const formSchema = z.object({
   name: z.string().min(1, { message: "Companion is required." }),
@@ -47,14 +48,14 @@ const CompanionForm = () => {
   });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    // const companion = await createCompanion(values);
+    const companion = await createCompanion(values);
 
-    // if (companion) {
-    //   redirect(`/companions/${companion.id}`);
-    // } else {
-    //   console.log("Failed to create a companion");
-    //   redirect("/");
-    // }
+    if (companion) {
+      redirect(`/companions/${companion.id}`);
+    } else {
+      console.log("Failed to create a companion");
+      redirect("/");
+    }
   };
 
   return (
