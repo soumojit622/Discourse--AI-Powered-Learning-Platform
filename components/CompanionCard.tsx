a752a5a9cee98afd4e2022abeadc95eb14135c91
Soumojit Banerjee <soumojitbanerjee22@gmail.com>
2025-05-31 11:04:23 +0530

    feat: 🛡️ Sentry setup, 🧠 session history, 👤 profile page, 🔐 subscription checks, 📌 bookmarks, 🚀 deployment completed

--- components/CompanionCard.tsx
+++ components/CompanionCard.tsx
@@ -1,9 +1,6 @@
 "use client";
-import { removeBookmark } from "@/lib/actions/companion.actions";
-import { addBookmark } from "@/lib/actions/companion.actions";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 interface CompanionCardProps {
   id: string;
@@ -22,30 +19,11 @@ const CompanionCard = ({
   subject,
   duration,
   color,
-  bookmarked,
 }: CompanionCardProps) => {
-  const pathname = usePathname();
-  const handleBookmark = async () => {
-    if (bookmarked) {
-      await removeBookmark(id, pathname);
-    } else {
-      await addBookmark(id, pathname);
-    }
-  };
   return (
     <article className="companion-card" style={{ backgroundColor: color }}>
       <div className="flex justify-between items-center">
         <div className="subject-badge">{subject}</div>
-        <button className="companion-bookmark" onClick={handleBookmark}>
-          <Image
-            src={
-              bookmarked ? "/icons/bookmark-filled.svg" : "/icons/bookmark.svg"
-            }
-            alt="bookmark"
-            width={12.5}
-            height={15}
-          />
-        </button>
       </div>
 
       <h2 className="text-2xl font-bold">{name}</h2>
